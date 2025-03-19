645fa44083f88a39b3387e0b0fcec92ff6e61f2a
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-20 00:52:21 +0100

    Initial commit

---
+++ src/main/java/de/flowsuite/mailflowapi/MailFlowApiApplication.java
@@ -0,0 +1,13 @@
+package de.flowsuite.mailflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailFlowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailFlowApiApplication.class, args);
+    }
+
+}
