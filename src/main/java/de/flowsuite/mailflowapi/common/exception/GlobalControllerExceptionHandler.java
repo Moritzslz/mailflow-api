34329d897caeee28c8c448a924a26a0502839afb
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-21 00:58:23 +0100

    Added GlobalControllerExceptionHandler

---
+++ src/main/java/de/flowsuite/mailflowapi/common/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package de.flowsuite.mailflowapi.common.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    // TODO
+}
