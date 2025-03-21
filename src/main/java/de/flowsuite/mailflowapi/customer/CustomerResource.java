14be4f9eb44d39b177f7a4164c66be75fe2a688e
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-21 02:50:59 +0100

    Added /api prefix

--- src/main/java/de/flowsuite/mailflowapi/customer/CustomerResource.java
+++ src/main/java/de/flowsuite/mailflowapi/customer/CustomerResource.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/customers")
+@RequestMapping("/api/v1/customers")
 class CustomerResource {
 
     private final CustomerService customerService;
