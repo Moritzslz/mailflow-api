2f595b86a1bdd67918423cc467158b1b558e29fe
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-03-23 04:18:58 +0100

    WiP: User & Service JWT auth functionality

--- src/main/java/de/flowsuite/mailflowapi/security/JwtService.java
+++ src/main/java/de/flowsuite/mailflowapi/security/JwtService.java
@@ -1,7 +1,5 @@
 package de.flowsuite.mailflowapi.security;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -9,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.stream.Collectors;
 
 @Service
 class JwtService {
@@ -20,18 +17,14 @@ class JwtService {
         this.jwtEncoder = jwtEncoder;
     }
 
-    String generateToken(Authentication authentication) {
+    String generateToken(String subject, String scope) {
         ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/Berlin"));
-        String scope =
-                authentication.getAuthorities().stream()
-                        .map(GrantedAuthority::getAuthority)
-                        .collect(Collectors.joining(" "));
         JwtClaimsSet claims =
                 JwtClaimsSet.builder()
-                        .issuer("self")
+                        .issuer("mailflow-api")
                         .issuedAt(now.toInstant())
                         .expiresAt(now.plusHours(1).toInstant())
-                        .subject(authentication.getName())
+                        .subject(subject)
                         .claim("scope", scope)
                         .build();
         return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
