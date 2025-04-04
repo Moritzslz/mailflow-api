a9cac9e4dbd56d3b1443fd6ca43413a26d547fec
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-04-04 05:05:16 +0200

    Spotless

--- src/main/java/de/flowsuite/mailflowapi/common/entity/User.java
+++ src/main/java/de/flowsuite/mailflowapi/common/entity/User.java
@@ -1,6 +1,7 @@
 package de.flowsuite.mailflowapi.common.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
