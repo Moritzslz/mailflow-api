fbebffe8499e31b230965f7bca99376ee42239d5
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-04-07 00:42:35 +0200

    Implemented createUser logic

--- src/main/java/de/flowsuite/mailflowapi/user/UserResource.java
+++ src/main/java/de/flowsuite/mailflowapi/user/UserResource.java
@@ -1,5 +1,14 @@
 package de.flowsuite.mailflowapi.user;
 
+import de.flowsuite.mailflowapi.common.entity.User;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customers")
 class UserResource {
 
-    record CreateUserRequest() {}
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    ResponseEntity<User> createUser(@RequestBody @Valid CreateUserRequest request) {
+        return ResponseEntity.ok(userService.createUser(request));
+    }
+
+    record CreateUserRequest(
+            @NotNull Long customerId,
+            @NotBlank String firstName,
+            @NotBlank String lastName,
+            @NotBlank String emailAddress,
+            @NotBlank String password,
+            @NotBlank String confirmationPassword,
+            String phoneNumber,
+            String position,
+            boolean isSubscribedToNewsletter) {}
 }
--- src/main/java/de/flowsuite/mailflowapi/user/UserService.java
+++ src/main/java/de/flowsuite/mailflowapi/user/UserService.java
@@ -1,20 +1,30 @@
 package de.flowsuite.mailflowapi.user;
 
+import de.flowsuite.mailflowapi.common.entity.Authorities;
 import de.flowsuite.mailflowapi.common.entity.User;
+import de.flowsuite.mailflowapi.common.exception.EntityExistsException;
+import de.flowsuite.mailflowapi.common.util.AesUtil;
 import de.flowsuite.mailflowapi.common.util.HmacUtil;
+import de.flowsuite.mailflowapi.common.util.Util;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Service
 public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    UserService(UserRepository userRepository) {
+    UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,7 +34,7 @@ public class UserService implements UserDetailsService {
 
     public User getByEmailAddress(String emailAddress) {
         return userRepository
-                .findByEmailAddressHash(HmacUtil.hash(emailAddress))
+                .findByEmailAddressHash(HmacUtil.hash(emailAddress.toLowerCase()))
                 .orElseThrow(() -> new UsernameNotFoundException("User not found."));
     }
 
@@ -34,5 +44,51 @@ public class UserService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found."));
     }
 
-    // TODO for PUT endpoint: customerId cannot be updated
+    private String generateVerificationToken() {
+        String verificationToken;
+        do {
+            verificationToken = Util.generateRandomUrlSafeToken();
+        } while (userRepository.existsByVerificationToken(verificationToken));
+        return verificationToken;
+    }
+
+    User createUser(UserResource.CreateUserRequest request) {
+        String emailAddress = request.emailAddress().toLowerCase();
+
+        Util.validateEmailAddress(emailAddress);
+        UserUtil.validatePassword(request.password(), request.confirmationPassword());
+
+        String emailAddressHash = HmacUtil.hash(emailAddress);
+        if (userRepository.existsByEmailAddressHash(emailAddressHash)) {
+            throw new EntityExistsException(User.class.getSimpleName());
+        }
+
+        String passwordHash = passwordEncoder.encode(request.password());
+
+        String verificationToken = generateVerificationToken();
+        ZonedDateTime tokenExpiresAt =
+                ZonedDateTime.now(ZoneId.of("Europe/Berlin")).plusMinutes(30);
+
+        User user =
+                User.builder()
+                        .customerId(request.customerId())
+                        .firstName(AesUtil.encrypt(request.firstName()))
+                        .lastName(AesUtil.encrypt(request.lastName()))
+                        .emailAddressHash(emailAddressHash)
+                        .emailAddress(AesUtil.encrypt(emailAddress))
+                        .password(passwordHash)
+                        .phoneNumber(request.phoneNumber())
+                        .position(request.position())
+                        .role(Authorities.USER.getAuthority())
+                        .isAccountLocked(false)
+                        .isAccountEnabled(false)
+                        .isSubscribedToNewsletter(request.isSubscribedToNewsletter())
+                        .verificationToken(verificationToken)
+                        .tokenExpiresAt(tokenExpiresAt)
+                        .build();
+
+        // TODO send Double-Opt-In email
+
+        return userRepository.save(user);
+    }
 }
