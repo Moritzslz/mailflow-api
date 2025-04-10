c4cdd4585cfb0ca0c1078d1b552792dc934e7fc2
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-04-10 02:14:31 +0200

    Moved replacePlaceholder into MailUtil

--- src/main/java/de/flowsuite/mailflowapi/mail/MailUtil.java
+++ src/main/java/de/flowsuite/mailflowapi/mail/MailUtil.java
@@ -10,12 +10,17 @@ import java.util.stream.Collectors;
 
 class MailUtil {
 
-    public static String readFile(ResourceLoader resourceLoader, String classpath) {
+    static String readFile(ResourceLoader resourceLoader, String classpath) {
         Resource resource = resourceLoader.getResource(classpath);
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+        try (BufferedReader reader =
+                new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
             return reader.lines().collect(Collectors.joining("\n"));
         } catch (IOException e) {
             throw new RuntimeException("Failed to read file: " + classpath, e);
         }
+    }
+
+    static String replacePlaceholder(String template, String placeholder, String value) {
+        return template.replaceAll("\\{\\{" + placeholder + "\\}\\}", value);
     }
 }
