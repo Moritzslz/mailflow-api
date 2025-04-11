85548d948a701b9d52e96e688b8877a4f764aa78
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-04-11 16:11:08 +0200

    Implemented update lastCrawlAt and nextCrawlAt

--- src/main/java/de/flowsuite/mailflowapi/settings/SettingsService.java
+++ src/main/java/de/flowsuite/mailflowapi/settings/SettingsService.java
@@ -68,6 +68,17 @@ class SettingsService {
         settings.setImapPort(request.imapPort());
         settings.setSmtpPort(request.smtpPort());
 
+        if (request.lastCrawlAt() != null) {
+            if (settings.getLastCrawlAt() == null || request.lastCrawlAt().isAfter(settings.getLastCrawlAt())) {
+                settings.setLastCrawlAt(request.lastCrawlAt());
+            }
+        }
+        if (request.nextCrawlAt() != null) {
+            if ( settings.getNextCrawlAt() == null || request.nextCrawlAt().isAfter( settings.getNextCrawlAt())) {
+                settings.setNextCrawlAt(request.nextCrawlAt());
+            }
+        }
+
         return settingsRepository.save(settings);
     }
 
