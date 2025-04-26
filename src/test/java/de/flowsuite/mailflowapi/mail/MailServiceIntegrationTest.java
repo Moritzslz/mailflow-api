ac03b0c88a370f90c93fb6236b7bd88e625f00e9
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-04-27 00:43:42 +0200

    Renamed and style

---
+++ src/test/java/de/flowsuite/mailflowapi/mail/MailServiceIntegrationTest.java
@@ -0,0 +1,43 @@
+package de.flowsuite.mailflowapi.mail;
+
+import de.flowsuite.mailflowapi.BaseServiceTest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class MailServiceIntegrationTest extends BaseServiceTest {
+
+    private static final long USER_ID = 1L;
+    private static final String FIRST_NAME = "Vorname";
+    private static final String EMAIL_ADDRESS = "test@flow-suite.de";
+    private static final String TOKEN = "token";
+    private static final int TOKEN_TTL_HOURS = 6;
+    private static final int TOKEN_TTL_MINUTES = 30;
+
+    @Autowired private MailService mailService;
+
+    @Test
+    void testSendDoubleOptInEmail_success() {
+        mailService.sendDoubleOptInEmail(FIRST_NAME, EMAIL_ADDRESS, TOKEN, TOKEN_TTL_HOURS);
+    }
+
+    @Test
+    void testSendPasswordResetEmail_success() {
+        mailService.sendPasswordResetEmail(
+                USER_ID, FIRST_NAME, EMAIL_ADDRESS, TOKEN, TOKEN_TTL_MINUTES);
+    }
+
+    @Test
+    void testSendPasswordResetExpiredEmail_success() {
+        mailService.sendPasswordResetExpiredEmail(USER_ID, FIRST_NAME, EMAIL_ADDRESS);
+    }
+
+    @Test
+    void testSendWelcomeEmail() {
+        mailService.sendWelcomeEmail(USER_ID, FIRST_NAME, EMAIL_ADDRESS);
+    }
+}
