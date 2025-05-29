cc4284b6c418d7c93cf3f71a29058aeff2230dcb
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-30 01:40:20 +0200

    Implemented notifyRagService

--- src/main/java/de/flowsuite/mailflow/api/ragurl/RagUrlResource.java
+++ src/main/java/de/flowsuite/mailflow/api/ragurl/RagUrlResource.java
@@ -4,23 +4,35 @@ import de.flowsuite.mailflow.common.entity.RagUrl;
 
 import jakarta.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/customers")
 class RagUrlResource {
 
-    private final RagUrlService ragUrlService;
+    private final Logger LOG = LoggerFactory.getLogger(RagUrlResource.class);
+    private static final String NOTIFY_RAG_URLS_URI = "/notifications/customers/{customerId}/rag-urls/{id}";
 
-    RagUrlResource(RagUrlService ragUrlService) {
+    private final RagUrlService ragUrlService;
+    private final RestClient ragServiceRestClient;
+
+    RagUrlResource(RagUrlService ragUrlService,
+                   @Qualifier("mailboxServiceRestClient") RestClient ragServiceRestClient) {
         this.ragUrlService = ragUrlService;
+        this.ragServiceRestClient = ragServiceRestClient;
     }
 
     @PostMapping("/{customerId}/rag-urls")
@@ -37,7 +49,7 @@ class RagUrlResource {
                         .buildAndExpand(createdRagUrl.getCustomerId(), createdRagUrl.getId())
                         .toUri();
 
-        // Todo notify rag service
+        CompletableFuture.runAsync(() -> notifyRagService(HttpMethod.POST, createdRagUrl, jwt));
 
         return ResponseEntity.created(location).body(createdRagUrl);
     }
@@ -62,8 +74,9 @@ class RagUrlResource {
             @PathVariable long id,
             @RequestBody @Valid RagUrl ragUrl,
             @AuthenticationPrincipal Jwt jwt) {
-        // Todo notify rag service
-        return ResponseEntity.ok(ragUrlService.updateRagUrl(customerId, id, ragUrl, jwt));
+        RagUrl updatedRagUrl = ragUrlService.updateRagUrl(customerId, id, ragUrl, jwt);
+        CompletableFuture.runAsync(() -> notifyRagService(HttpMethod.PUT, updatedRagUrl, jwt));
+        return ResponseEntity.ok(ragUrl);
     }
 
     @PutMapping("/{customerId}/rag-urls/{id}/crawl-status")
@@ -80,8 +93,19 @@ class RagUrlResource {
             @PathVariable long customerId,
             @PathVariable long id,
             @AuthenticationPrincipal Jwt jwt) {
-        ragUrlService.deleteRagUrl(customerId, id, jwt);
-        // Todo notify rag service
+        RagUrl deletedRagUrl = ragUrlService.deleteRagUrl(customerId, id, jwt);
+        CompletableFuture.runAsync(() -> notifyRagService(HttpMethod.DELETE, deletedRagUrl, jwt));
         return ResponseEntity.noContent().build();
+    }
+
+    private void notifyRagService(HttpMethod method, RagUrl ragUrl, Jwt jwt) {
+        LOG.debug("Notifying rag service of rag url change");
+
+        ragServiceRestClient
+                .method(method)
+                .uri(NOTIFY_RAG_URLS_URI, ragUrl.getCustomerId(), ragUrl.getId())
+                .body(ragUrl)
+                .retrieve()
+                .toBodilessEntity();
     }
 }
