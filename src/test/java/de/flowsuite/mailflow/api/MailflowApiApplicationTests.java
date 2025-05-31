78efe4bacbcc5a5d08cc6cf905f54147a6463c2a
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-31 19:36:00 +0200

    Disabled datasource config during test

--- src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
+++ src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
@@ -1,9 +1,12 @@
 package de.flowsuite.mailflow.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class MailflowApiApplicationTests {
 
     @Test
