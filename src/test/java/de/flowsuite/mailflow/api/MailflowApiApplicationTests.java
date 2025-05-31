b8279f2dc5897d5f894087a95f449809bff8ff9f
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-06-01 01:44:58 +0200

    Implemented TestSecurityConfig

--- src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
+++ src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
@@ -2,9 +2,11 @@ package de.flowsuite.mailflow.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 @SpringBootTest
 class MailflowApiApplicationTests {
 
