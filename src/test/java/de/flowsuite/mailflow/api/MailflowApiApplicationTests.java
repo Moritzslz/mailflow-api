6c5ee6216a07d4920a7e9df3ad89959662f6e3ba
Moritzslz <109749981+Moritzslz@users.noreply.github.com>
2025-05-31 23:40:37 +0200

    Test configuration

--- src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
+++ src/test/java/de/flowsuite/mailflow/api/MailflowApiApplicationTests.java
@@ -1,12 +1,11 @@
 package de.flowsuite.mailflow.api;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class MailflowApiApplicationTests {
 
     @Test
